80d18f2b7eec886204779dd84f2b176cbed06677
kickinbahk@gmail.com <kickinbahk@gmail.com>
2017-04-08 12:44:35 -0700

    create function to print statement

--- ClassCertificates/main.c
+++ ClassCertificates/main.c
@@ -8,8 +8,15 @@
 
 #include <stdio.h>
 
+void congratulateStudent(char *student, char *course, int numDays) {
+  printf("%s has done as much %s Programming as I could fit into %d days.\n", student, course, numDays);
+}
+
 int main(int argc, const char * argv[]) {
-  // insert code here...
-  printf("Hello, World!\n");
+  congratulateStudent("Kate", "Cocoa", 5);
+  congratulateStudent("Bo", "Objective-C", 2);
+  congratulateStudent("Mike", "Python", 5);
+  congratulateStudent("Liz", "iOS", 5);
+  
   return 0;
 }
